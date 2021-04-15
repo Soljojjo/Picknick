97005d788a0b91a7087f2e1858b76b3b67e3e5f4
Tengill91 <robban_t@hotmail.com>
2021-04-15 11:55:29 +0200

    nu ser det bra ut.. hyffsat i alla fall

--- src/project/MainMenu.js
+++ src/project/MainMenu.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Sidebar from './Sidebar';
 import Sidebar2 from './Sidebar2';
 
+
 const TvPrograms = () => {
 
     const url = "https://tv-api-p2x2o.ondigitalocean.app/SVT 1.json"
@@ -19,29 +20,60 @@ const TvPrograms = () => {
 // Can only return one object, encapsulated in a DIV here
     return (
         <div>
-        <div style={{ display: "grid", gridTemplateColumns: "repeat(4, 1fr)", gridGap: 20 }}>
+
+
+        
+        
+
+
+
+
+
+
             
-            <div> column 1
-                
-            </div>
-                
-            <div> column 2
-            <Sidebar /> 
 
-            </div>
-
-            <div> column 3
+    <div class= "header">
+       <h1>Upptäck äventyren nära dig</h1>
+       <p class="txt_1">Hitta ett utflyktsmål där det går att byta blöjor? Värma vällingen? </p> 
+       <p class="txt_2">Använd picknick för att hitta utflyktsmål dom passar hela familjen</p>
+    </div>
+    
+    <div class="container">
+        <div class="left-column">
+            <h2>Left</h2>
+            
+        </div>
+        
+        
+        <div class="center-column1">
+            <h2>Utflykt1</h2>
+            <Sidebar />
+        </div>
+        <div class="center-column2">
+            <h2>Utflykt2</h2>
             <Sidebar2 />
-            </div>
+        </div>
+        <div class="right-column">
+            <h2>Right</h2>
+        </div>
+    </div>
+        <div class="footer">
+            <p>&copy; All rights reserved.</p>
+        </div>
+ 
+    
 
-            <div> column 4
-            
-                
-            </div>
-                
-            
-        </div>
-        </div>
+</div>
 );
 };
+
 export default TvPrograms;
+
+
+
+
+
+
+
+
+
