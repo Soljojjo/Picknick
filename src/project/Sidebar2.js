ca61750b23e7e314bc8d3cf0b3ffa5022e5fbb10
Tengill91 <robban_t@hotmail.com>
2021-04-15 22:34:36 +0200

    shadow and border

--- src/project/Sidebar2.js
+++ src/project/Sidebar2.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Hello from './sayHello'
 
 
 
@@ -6,7 +7,7 @@ import React from 'react'
 const Sidebar = () =>{
     return(
         <div class="menu">
-            <a class="menu__link" href="">Tyresta Nationalpark</a> 
+            <a class="menu__link" onClick= {Hello} href="">Tyresta Nationalpark</a> 
             <a class="menu__link" href="">Birka, Mörkö</a>
             <a class="menu__link" href="">Grottan på Mörkö</a>        
         </div>
