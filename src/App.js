89ebe029a16e81d8e2fb770967a662c9701873ff
Tengill91 <robban_t@hotmail.com>
2021-04-20 00:52:49 +0200

    fixat routing!

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import React from 'react';
 import MainMenu from './project/MainMenu';
+import InfoPage from './project/InfoPage';
+import { Route, BrowserRouter as Router, Switch, Link } from "react-router-dom";
+
 
 function App() {
-  
+
   return (
-    <div>
-      
+    <Router>
 
-      <MainMenu />
-      
+      <div>
+      <Switch>
+          <Route path="/" exact component={MainMenu} />
+          <Route path="/InfoPage" component={InfoPage} />
+        </Switch>
+        <p>testar routing</p>
+        <nav>
+            <ul>
+                <li>
+                    <Link to= "/InfoPage">InfoPage</Link>
+                </li>
+                <li>
+                    <Link to= "/">MainMenu</Link>
+                </li>
+            </ul>
+        </nav>
 
-    </div>
+        
+      </div>
+
+    </Router>
   );
 }
 
---
+++ src/project/InfoPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function InfoPage() {
+    return (
+        <div>
+            <h1>Muy Bueno Informatione!</h1>
+        </div>
+    )
+}
