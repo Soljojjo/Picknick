3708ac1df701a019aa8bdd0553ea97bd92db8d15
Johanna <johannatovebergstrom@gmail.com>
2021-04-20 11:36:54 +0200

    Förstasida anpassad till mobil

--- src/project/MainMenu.js
+++ src/project/MainMenu.js
@@ -21,34 +21,31 @@ const MainMenu = () => {
     return (
         <div>
 
+            
+
+            <div class="header">
             <div class="logoPicknick">
                 <img src="./images/picknick.jpg" alt=" logo " />
             </div>
-
-            <div class="header">
-            
-                <h1>Upptäck äventyren nära dig</h1>
-                <p class="body_copy">Hitta ett utflyktsmål där det går att byta blöjor? Värma vällingen? <br />
-                Använd Picknick för att hitta utflyktsmål som passar hela familjen.
-                </p>
+                
 
             </div>
-            <p class="family">
-                <img src="./icons/family_friendly.png" alt="<h2>Familjevänliga utflyktsmål</h2>" width="800" />
-            </p>
+           
 
             <div class="container">
-                <div class="left-column">
-                    <h2></h2>
-                </div>
+                {/* <div>
+                <img class="menupicture" src="./images/water.jpg" alt="girl at water"/>
+                </div> */}
+            <h1 class="containertext">Upptäck äventyren <br></br>nära dig</h1>
+                <p class="body_copy">Hitta utflyktsmål anpassade för hela <br></br>familjen         
+                </p> 
+            </div> 
 
+            <div class="container2">   
                 <div class="center-column">
                     <Destinations />
                 </div>
 
-                <div class="right-column">
-                    <h2></h2>
-                </div>
             </div>
             <div class="footer">
                 <p>&copy; All rights reserved.</p>
