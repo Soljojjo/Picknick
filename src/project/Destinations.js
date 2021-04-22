c5e44b9901904f6ca9c44620aa7aeea08b90a8f2
Greger Lindkvist <greg.kvist@gmail.com>
2021-04-22 20:34:06 +0200

    Updated Places.json, Destinations.js using some json data

--- src/project/Destinations.js
+++ src/project/Destinations.js
@@ -1,23 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import { Route, BrowserRouter as Router, Switch, Link } from "react-router-dom";
-
+import Places from './Places.json';
 
 function Destinations() {
-    
-   const [id, setId] = useState(1);
 
-    
+    const [id, setId] = useState(1);
+
+
     //Ändra namn på menu och menu__link
     return (
         <div class="menu">
             <div>
-                
-            </div>
-            <Link to="/InfoPage" className="menu__link" > Bergianska trädgården</Link>
-            <Link to= "/InfoPage" class = "menu__link">Ekospåret på Artipelag</Link>
-            <Link to= "/InfoPage" class = "menu__link">Ekorrstigen i Huddinge</Link>
-            
-        </div>
+                {Places.map(place =>
+                    <div>
+                        <Link to="/InfoPage"  className="menu__link"> <img src={place.card}/> {place.name}</Link>
+                    </div>
+                )}
+            </div >
+        </div >
     )
 }
 //exporteras som en JavaScript komponent
@@ -25,7 +25,16 @@ export default Destinations;
 
 
 
-        /*<a class="menu__link" href="" onClick={handleClick}>Bergianska trädgården <ion-icon class="menu__icon" name="arrow-forward-outline"></ion-icon></a>
-            <a class="menu__link" href="" onClick={handleClick}>Ekospåret på Artipelag<ion-icon class="menu__icon" name="arrow-forward-outline"></ion-icon></a>
-            <a class="menu__link" href="" onClick={handleClick}>Ekorrstigen i Huddinge<ion-icon class="menu__icon" name="arrow-forward-outline"></ion-icon></a>
-            */
+/*<a class="menu__link" href="" onClick={handleClick}>Bergianska trädgården <ion-icon class="menu__icon" name="arrow-forward-outline"></ion-icon></a>
+    <a class="menu__link" href="" onClick={handleClick}>Ekospåret på Artipelag<ion-icon class="menu__icon" name="arrow-forward-outline"></ion-icon></a>
+    <a class="menu__link" href="" onClick={handleClick}>Ekorrstigen i Huddinge<ion-icon class="menu__icon" name="arrow-forward-outline"></ion-icon></a>
+    */
+
+/*
+</div>
+        <Link to="/InfoPage" className="menu__link" > {place.name}</Link>
+        <Link to="/InfoPage" class="menu__link">Ekospåret på Artipelag</Link>
+        <Link to="/InfoPage" class="menu__link">Ekorrstigen i Huddinge</Link>
+
+    </div >
+*/
