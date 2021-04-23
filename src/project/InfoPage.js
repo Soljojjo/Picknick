57b88fd38cef58d2318c1466b1e019dbc8fd2b8e
Johanna <johannatovebergstrom@gmail.com>
2021-04-23 10:29:44 +0200

    Adjusted distance between blocks CSS

--- src/project/InfoPage.js
+++ src/project/InfoPage.js
@@ -11,8 +11,8 @@ const InfoPage = () => {
 
     function handleClick() {
         console.log('The link was clicked. ');
-        <Link to= "/"></Link>;
-      }
+        <Link to="/"></Link>;
+    }
 
 
 
@@ -21,50 +21,48 @@ const InfoPage = () => {
     return (
         <div>
 
-            
+
 
             <div class="header">
-            <div>
-                <Link to="/"> <img class= "logoPicknick"  src="./icons/picknickLogoAndText.png" alt=" logo " /></Link>
-            </div>
-                
+                <div>
+                    <Link to="/"> <img class="logoPicknick" src="./icons/picknickLogoAndText.png" alt=" logo " /></Link>
+                </div>
+
 
             </div>
-           
+
 
             <div class="infoContainer">
-                <div>
-                <Link class="link" name to= "/"></Link>
-                </div>
+               
                 {Places.filter((place) => place.id === 1).map(place =>
-                <div> 
-                <h1>{place.name}</h1>
-                </div>
+                    <div>
+                        <h1>{place.name}</h1>
+                    </div>
                 )}
                 <div>
-                <div class="menupicture" />
-                </div> 
+                    <div class="menupicture" />
+                </div>
 
-            </div> 
-            
-            <div class="infoContainer2"> 
+            </div>
 
-            <p class="body_copy"></p> 
-                
+            <div class="infoContainer2">
+
+                <p class="body_copy"></p>
+
             </div>
 
             <div class="infoContainer3">
-                <p class = "body_copy"><b>OM UTFLYKTSMÅLET</b>. I Bergianska trädgården kan du med barnvagnen promenera och upptäcka växter från hela världen.
+                <p class="body_copy"><b>OM UTFLYKTSMÅLET</b>. I Bergianska trädgården kan du med barnvagnen promenera och upptäcka växter från hela världen.
 
                 Besök gärna växthuset där exotiska växter som vanligen hittas i regnskogar och öknen.
 
-                <br/>
-                <br/> 
+                <br />
+                    <br />
                 Ta med dig barnen på utomhusaktiviteter
                 som ni alla kommer gilla och avsluta med en god lunch i vår restaurang. </p>
-                
+
             </div>
-            
+
             <div class="footer">
                 <p>&copy; All rights reserved.</p>
             </div>
