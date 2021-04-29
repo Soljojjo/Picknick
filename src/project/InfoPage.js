2d86745ccf8b3ae214b819b7e5b81871c0fbd575
Greger Lindkvist <greg.kvist@gmail.com>
2021-04-29 15:55:54 +0200

    Changed class to className in InfoPage.js

--- src/project/InfoPage.js
+++ src/project/InfoPage.js
@@ -17,16 +17,16 @@ const InfoPage = () => {
 
     // Can only return one object, encapsulated in a DIV here
     return (
-        <div class="column_all">
+        <div className="column_all">
 
-            <div class="header">
+            <div className="header">
                 <div>
-                    <Link to="/"> <img class="logoPicknick" src="../icons/picknickLogoAndText.png" alt=" logo " /></Link>
+                    <Link to="/"> <img classname="logoPicknick" src="../icons/picknickLogoAndText.png" alt=" logo " /></Link>
                 </div>
 
             </div>
 
-            <div class="infoContainer">
+            <div className="infoContainer">
                 <div>{/* från json filen filtreras info där id i jsonfilen = id från Url
                 map tar all info och lägger i place, namnet hämtas från place.
                 samma sak görs för bilden och beskrivning i nästa div */}
@@ -39,83 +39,83 @@ const InfoPage = () => {
                 <div>
                     {Places.filter((place) => place.id == id).map(place =>
                         <div>
-                            <img class="infopicture" src={"../places/" + place.img3} alt={place.name} />
+                            <img classname="infopicture" src={"../places/" + place.img3} alt={place.name} />
                         </div>
                     )}
                 </div>
 
             </div>
 
-            <div class="infoContainer2">
+            <div className="infoContainer2">
                 <p>På platsen:</p>
                 <div>
                     {Places.filter((place) => place.id == id).map(place =>
                         <div>
                             {place.changingDiaper ? (
-                                <button className="infobutton"><span class="filtertext-middle">Skötbord</span> </button>) :
+                                <button className="infobutton"><span classname="filtertext-middle">Skötbord</span> </button>) :
                                 ("")
                             }
 
                             {place.parking ? (
-                                <button className="infobutton"><span class="filtertext-middle">Parkering</span> </button>) :
+                                <button className="infobutton"><span classname="filtertext-middle">Parkering</span> </button>) :
                                 ("")
                             }
 
                             {place.wc ? (
-                                <button className="infobutton"><span class="filtertext-middle">Wc</span> </button>) :
+                                <button className="infobutton"><span classname="filtertext-middle">Wc</span> </button>) :
                                 ("")
                             }
 
                             {place.fourH ? (
-                                <button className="infobutton"><span class="filtertext-middle">4H</span> </button>) :
+                                <button className="infobutton"><span classname="filtertext-middle">4H</span> </button>) :
                                 ("")
                             }
 
                             {place.pramFriendly ? (
-                                <button className="infobutton"><span class="filtertext-middle">Barnvagnsvänligt</span> </button>) :
+                                <button className="infobutton"><span classname="filtertext-middle">Barnvagnsvänligt</span> </button>) :
                                 ("")
                             }
                             {place.cafe ? (
-                                <button className="infobutton"><span class="filtertext-middle">Kiosk/cafe</span> </button>) :
+                                <button className="infobutton"><span classname="filtertext-middle">Kiosk/cafe</span> </button>) :
                                 ("")
                             }
                             {place.fireplace ? (
-                                <button className="infobutton"><span class="filtertext-middle">Grillplats</span> </button>) :
+                                <button className="infobutton"><span classname="filtertext-middle">Grillplats</span> </button>) :
                                 ("")
                             }
 
                             {place.playground ? (
-                                <button className="infobutton"><span class="filtertext-middle">Lekplats</span> </button>) :
+                                <button className="infobutton"><span classname="filtertext-middle">Lekplats</span> </button>) :
                                 ("")
                             }
 
                             {place.microwave ? (
-                                <button className="infobutton"><span class="filtertext-middle">Mikrovågsugn</span> </button>) :
+                                <button className="infobutton"><span classname="filtertext-middle">Mikrovågsugn</span> </button>) :
                                 ("")
                             }
 
                             {place.beach ? (
-                                <button className="infobutton"><span class="filtertext-middle">Badplats</span> </button>) :
+                                <button className="infobutton"><span classname="filtertext-middle">Badplats</span> </button>) :
                                 ("")
                             }
 
                             {place.trails ? (
-                                <button className="infobutton"><span class="filtertext-middle">Promenadstigar</span> </button>) :
+                                <button className="infobutton"><span classname="filtertext-middle">Promenadstigar</span> </button>) :
                                 ("")
                             }
 
                             {place.elevatorRamp ? (
-                                <button className="infobutton"><span class="filtertext-middle">Hiss/ramp</span> </button>) :
+                                <button className="infobutton"><span classname="filtertext-middle">Hiss/ramp</span> </button>) :
                                 ("")
                             }
 
                             {place.picnicCompatible ? (
-                                <button className="infobutton"><span class="filtertext-middle">Picknickområde</span> </button>) :
+                                <button className="infobutton"><span classname="filtertext-middle">Picknickområde</span> </button>) :
                                 ("")
                             }
 
                             {place.restaurant ? (
-                                <button className="infobutton"><span class="filtertext-middle">Restaurang</span> </button>) :
+                                <button className="infobutton"><span classname="filtertext-middle">Restaurang</span> </button>) :
                                 ("")
                             }
                         </div>
@@ -124,7 +124,7 @@ const InfoPage = () => {
 
             </div>
 
-            <div class="infoContainer3">
+            <div className="infoContainer3">
                 <div>
                     {Places.filter((place) => place.id == id).map(place =>
                         <div>
@@ -135,7 +135,7 @@ const InfoPage = () => {
 
             </div>
 
-            <div class="footer">
+            <div className="footer">
                 <p>&copy; All rights reserved.</p>
             </div>
 
@@ -146,7 +146,7 @@ const InfoPage = () => {
 export default InfoPage;
 
 /*
-<p class="family">
+<p classname="family">
 <img src="./icons/family_friendly_2440px.png" alt="<h2>Familjevänliga utflyktsmål</h2>" />
 </p>
 */
