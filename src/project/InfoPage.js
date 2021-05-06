1ddabed69da8527f498b62aa32c183394c070bc2
Greger Lindkvist <greg.kvist@gmail.com>
2021-05-06 16:54:05 +0200

    InfoPage lists evereything on same row

--- src/project/InfoPage.js
+++ src/project/InfoPage.js
@@ -19,96 +19,81 @@ const InfoPage = () => {
                 </div>
 
             </div>
-
-            <div className="infoContainer">
-                <div>{/* från json filen filtreras info där id i jsonfilen = id från Url
-                map tar all info och lägger i place, namnet hämtas från place.
-                samma sak görs för bilden och beskrivning i nästa div */}
-                    {Places.filter((place) => place.id === parseInt(id)).map(place =>
-                        <li key={place.id.toString()}>
-                            <h1>{place.name}</h1>
-                        </li>
-                    )}
-                </div>
+            {Places.filter((place) => place.id === parseInt(id)).map(place =>
                 <div>
-
-                    {/* Filtering out the place with the right id */}
-                    {Places.filter((place) => place.id === parseInt(id)).map(place =>
-                        <li key={place.id.toString()}>
-                            <img className="infopicture" src={"../places/" + place.img3} alt={place.name} />
-                        </li>
-                    )}
-                </div>
-            </div>
-
-            <div className="infoContainer2">
-                <p>Dina val:</p>
-                {/* Adding non-pressable buttons for the filter choices that have been made */}
-                {search.match("changingDiaper") ? (<button className="button_on">Skötbord</button>) : ""}
-                {search.match("parking") ? (<button className="button_on">Parkering</button>) : ""}
-                {search.match("wc") ? (<button className="button_on">WC</button>) : ""}
-                {search.match("fourH") ? (<button className="button_on">4h-gård</button>) : ""}
-                {search.match("pramFriendly") ? (<button className="button_on">Barnvagnsvänligt</button>) : ""}
-                {search.match("cafe") ? (<button className="button_on">Kiosk/café </button>) : ""}
-                {search.match("fireplace") ? (<button className="button_on">Grillplats </button>) : ""}
-                {search.match("playground") ? (<button className="button_on">Lekplats </button>) : ""}
-                {search.match("microwave") ? (<button className="button_on">Micro</button>) : ""}
-                {search.match("beach") ? (<button className="button_on">Badplats</button>) : ""}
-                {search.match("trails") ? (<button className="button_on">Promenadstigar</button>) : ""}
-                {search.match("elevatorRamp") ? (<button className="button_on">Hiss/ramp</button>) : ""}
-                {search.match("picnicCompatible") ? (<button className="button_on">Picknickområde</button>) : ""}
-                {search.match("restaurant") ? (<button className="button_on">Restaurang</button>) : ""}
-            </div>
-
-            <div className="infoContainer3">
-                <p>Också på platsen:</p>
-
-
-                {Places.filter((place) => place.id === parseInt(id)).map(place =>
                     <li key={place.id.toString()}>
+                        <div className="infoContainer">
+                            <div>{/* från json filen filtreras info där id i jsonfilen = id från Url
+                                 map tar all info och lägger i place, namnet hämtas från place.
+                                 samma sak görs för bilden och beskrivning i nästa div */}
 
-                        {/* Adding non-pressable buttons for the facilities that the place has, that were NOT included in the filter requiremnts */}
-                        {(place.changingDiaper && !search.match("changingDiaper")) ? <button className="button_off">Skötbord </button> : ""}
-                        {(place.parking && !search.match("parking")) ? <button className="button_off">Parkering </button> : ""}
-                        {(place.wc && !search.match("wc")) ? <button className="button_off">WC </button> : ""}
-                        {(place.fourH && !search.match("fourH")) ? <button className="button_off">4H </button> : ""}
-                        {(place.pramFriendly && !search.match("pramFriendly")) ? <button className="button_off">Barnvagnsvänligt </button> : ""}
-                        {(place.cafe && !search.match("cafe")) ? <button className="button_off">Kiosk/cafe </button> : ""}
-                        {(place.fireplace && !search.match("fireplace")) ? <button className="button_off">Grillplats </button> : ""}
-                        {(place.playground && !search.match("playground")) ? <button className="button_off">Lekplats </button> : ""}
-                        {(place.microwave && !search.match("microwave")) ? <button className="button_off">Mikrovågsugn </button> : ""}
-                        {(place.beach && !search.match("beach")) ? <button className="button_off">Badplats </button> : ""}
-                        {(place.trails && !search.match("trails")) ? <button className="button_off">Promenadstigar </button> : ""}
-                        {(place.elevatorRamp && !search.match("elevatorRamp")) ? <button className="button_off">Hiss/ramp </button> : ""}
-                        {(place.picnicCompatible && !search.match("picnicCompatible")) ? <button className="button_off">Picknickområde </button> : ""}
-                        {(place.restaurant && !search.match("restaurant")) ? <button className="button_off">Restaurang </button> : ""}
+                                <h1>{place.name}</h1>
+
+                            </div>
+                            <div>
+                                <img className="infopicture" src={"../places/" + place.img3} alt={place.name} />
+                            </div>
+                        </div>
+
+                        <div className="infoContainer2">
+                            <p>Dina val:</p>
+                            {/* Adding non-pressable buttons for the filter choices that have been made */}
+                            {search.match("changingDiaper") ? (<button className="button_on">Skötbord</button>) : ""}
+                            {search.match("parking") ? (<button className="button_on">Parkering</button>) : ""}
+                            {search.match("wc") ? (<button className="button_on">WC</button>) : ""}
+                            {search.match("fourH") ? (<button className="button_on">4h-gård</button>) : ""}
+                            {search.match("pramFriendly") ? (<button className="button_on">Barnvagnsvänligt</button>) : ""}
+                            {search.match("cafe") ? (<button className="button_on">Kiosk/café </button>) : ""}
+                            {search.match("fireplace") ? (<button className="button_on">Grillplats </button>) : ""}
+                            {search.match("playground") ? (<button className="button_on">Lekplats </button>) : ""}
+                            {search.match("microwave") ? (<button className="button_on">Micro</button>) : ""}
+                            {search.match("beach") ? (<button className="button_on">Badplats</button>) : ""}
+                            {search.match("trails") ? (<button className="button_on">Promenadstigar</button>) : ""}
+                            {search.match("elevatorRamp") ? (<button className="button_on">Hiss/ramp</button>) : ""}
+                            {search.match("picnicCompatible") ? (<button className="button_on">Picknickområde</button>) : ""}
+                            {search.match("restaurant") ? (<button className="button_on">Restaurang</button>) : ""}
+                        </div>
+
+                        <div className="infoContainer3">
+                            <p>Också på platsen:</p>
+
+
+                            {/* Adding non-pressable buttons for the facilities that the place has, that were NOT included in the filter requiremnts */}
+                            {(place.changingDiaper && !search.match("changingDiaper")) ? <button className="button_off">Skötbord </button> : ""}
+                            {(place.parking && !search.match("parking")) ? <button className="button_off">Parkering </button> : ""}
+                            {(place.wc && !search.match("wc")) ? <button className="button_off">WC </button> : ""}
+                            {(place.fourH && !search.match("fourH")) ? <button className="button_off">4H </button> : ""}
+                            {(place.pramFriendly && !search.match("pramFriendly")) ? <button className="button_off">Barnvagnsvänligt </button> : ""}
+                            {(place.cafe && !search.match("cafe")) ? <button className="button_off">Kiosk/cafe </button> : ""}
+                            {(place.fireplace && !search.match("fireplace")) ? <button className="button_off">Grillplats </button> : ""}
+                            {(place.playground && !search.match("playground")) ? <button className="button_off">Lekplats </button> : ""}
+                            {(place.microwave && !search.match("microwave")) ? <button className="button_off">Mikrovågsugn </button> : ""}
+                            {(place.beach && !search.match("beach")) ? <button className="button_off">Badplats </button> : ""}
+                            {(place.trails && !search.match("trails")) ? <button className="button_off">Promenadstigar </button> : ""}
+                            {(place.elevatorRamp && !search.match("elevatorRamp")) ? <button className="button_off">Hiss/ramp </button> : ""}
+                            {(place.picnicCompatible && !search.match("picnicCompatible")) ? <button className="button_off">Picknickområde </button> : ""}
+                            {(place.restaurant && !search.match("restaurant")) ? <button className="button_off">Restaurang </button> : ""}
+
+                        </div>
+
+                        <div className="infoContainer4">
+                            <div>
+
+                                <p>{place.description}</p>
+
+                            </div>
+
+                        </div>
+
+                        <div className="footer">
+
+                            <a className="mapbutton" href={place.mapsLink} style={{ textDecoration: 'none' }} target="_blank">ÖPPNA I KARTTJÄNST</a>
+
+
+                        </div>
                     </li>
-                )}
-
-
-            </div>
-
-            <div className="infoContainer4">
-                <div>
-                    {Places.filter((place) => place.id === parseInt(id)).map(place =>
-                        <li key={place.id.toString()}>
-                            <p>{place.description}</p>
-                        </li>
-                    )}
                 </div>
-
-            </div>
-
-            <div className="footer">
-                {Places.filter((place) => place.id === parseInt(id)).map(place =>
-                    <li key={place.id.toString()}>
-
-                        <a className="mapbutton" href={place.mapsLink} style={{ textDecoration: 'none' }} target="_blank">ÖPPNA I KARTTJÄNST</a>
-
-                    </li>
-                )}
-            </div>
-
+            )}
         </div>
     );
 };
